bb565bbefaec3a237191b7fd140faaf69eb12b7e
Marcelo Saldanha <marcelo.sfilho97@gmail.com>
2025-11-14 18:51:53 -0300

    urls for auth in DRF

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from oauth2_provider import urls as oauth2_urls
 
+'''
 urlpatterns = [
-    path('o/', include(oauth2_urls, namespace='oauth2_provider')),
+    #! Define your API endpoints here
+    path('example/', include('api.example_urls')),
 ]
+'''
--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from oauth2_provider import urls as oauth2_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('o/', include(oauth2_urls, namespace='oauth2_provider')),
     path('api/', include('api.urls'), name='api'),
 ]
+
