7693fdcd9951796ccbf115d2a96fb6390c68b9f5
Marcelo Saldanha <marcelo.sfilho97@gmail.com>
2025-11-14 19:22:48 -0300

    protect endpoints

--- api/views.py
+++ api/views.py
@@ -1,11 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+
+
+#! Import OAuth2 permission classes
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
+
 
 class ExampleAPIView(APIView):
-    permission_classes = [IsAuthenticated]
+    #! Protecting the endpoint with OAuth2 scopes (scope: read)
+    permission_classes = [TokenHasScope]
+    required_scopes = ['read']
 
     def get(self, request):
+      #! the information to be returned by the API after token validation
         data = {
             "message": "Hello, this is a protected API endpoint!"
         } 
